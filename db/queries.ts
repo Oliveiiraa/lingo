cd13a200dfffc743f3d9856565985d9cc1be6dd1
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2024-03-23 18:48:17 -0300

    feat: add get user progress

--- db/queries.ts
+++ db/queries.ts
@@ -1,6 +1,23 @@
-import {cache} from 'react'
-import db from '@/db/drizzle'
+import { cache } from "react";
+import db from "@/db/drizzle";
+import { auth } from "@clerk/nextjs";
+import { eq } from "drizzle-orm";
+import { userProgress } from "./schema";
 
+export const getUserProgress = cache(async () => {
+  const { userId } = auth();
+
+  if (!userId) return null;
+
+  const data = await db.query.userProgress.findFirst({
+    where: eq(userProgress.userId, userId),
+    with: {
+      activeCourse: true,
+    },
+  });
+
+  return data;
+});
 export const getCourses = cache(async () => {
   return await db.query.courses.findMany();
-})
+});
