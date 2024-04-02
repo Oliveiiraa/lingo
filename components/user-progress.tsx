b735e2e2cdf8999cbec34921217a5ae80626857f
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2024-04-02 09:03:54 -0300

    feat: add script seed

--- components/user-progress.tsx
+++ components/user-progress.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { InfinityIcon } from "lucide-react";
+import { courses } from "@/db/schema";
 
 type Props = {
-  activeCourse: { imageSrc: string; title: string }; //TODO: Replace with DB types
+  activeCourse: typeof courses.$inferSelect;
   hearts: number;
   points: number;
   hasActiveSubscription: boolean;
